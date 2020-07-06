707b81c385ca8f6ee32ff22aa7d65c9b9aa3789b
DANBI HWANG <hdb@MacBook-Pro.local>
2020-07-07 01:53:13 +0900

    Updates

---
+++ precache-manifest.6bfa575ffadef3156c777232a1ad909c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c57c4b675957b0a33a89b2cf90a52e6",
+    "url": "/sweetrainnnnn/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7c62a24bf9ff0a086ba0",
+    "url": "/sweetrainnnnn/movie_app_2020/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/sweetrainnnnn/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sweetrainnnnn/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c62a24bf9ff0a086ba0",
+    "url": "/sweetrainnnnn/movie_app_2020/static/js/main.9314b88a.chunk.js"
+  },
+  {
+    "revision": "943bab999811cf3eb642",
+    "url": "/sweetrainnnnn/movie_app_2020/static/js/runtime-main.0ff78ad5.js"
+  }
+]);
